2fda41182b4282bbef4c2cc547a102c430f491c3
rhysseddon <rhysseddon@hotmail.com>
2021-01-29 12:28:09 +0000

    Add user profile page

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ def register():
         # put the user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Welcome to the club!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -60,17 +62,28 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
-                #invalid password
+                # invalid password
                 flash("Incorrect Username and/or password")
                 return redirect(url_for("login"))
 
         else:
-            #username doesnt exist
+            # username doesnt exist
             flash("Incorrect Username and/or Password")
-            
+
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # get session users username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
